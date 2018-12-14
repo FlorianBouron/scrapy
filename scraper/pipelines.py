8dc2b1cc7302a73da9c36895b0e55456ffdede0f
Florian Bouron <bouron.florian@yahoo.fr>
2018-12-14 10:28:39 +0100

    Add a spider with item pipelines

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -6,6 +6,17 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ScraperPipeline(object):
+class MacbookCheck(object):
     def process_item(self, item, spider):
+        if ('macbook' not in item['title'].lower()):
+        	item['title'] = 'Non-Macbook'
         return item
+
+class MarkAsViable(object):
+	def process_item(self, item, spider):
+		if item['title'] != 'Non-Macbook':
+			print('\n\n OPTION FOUND!!')
+			print('Link: ', item['link'])
+			print('Price: ', item['price'])
+			print('Title: ', item['title'], '\n')
+		return item
---
+++ scraper/spiders/spider_pipeline.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose
+from scrapy.loader.processors import TakeFirst
+from scraper.items import ProductItemLoader
+
+class ProductDetails(scrapy.Spider):
+	name = 'product_scraper_pipeline'
+	start_urls = ['https://www.amazon.fr/s/ref=nb_sb_noss_2?'\
+					'url=search-alias%3Daps&field-keywords=macbook']
+
+	def parse(self, reponse):
+		search_results = reponse.css('ul.s-result-list > li')
+		for product in search_results:
+			product_loader = ItemLoader(item = ProductItemLoader(), selector = product)
+			product_loader.default_output_processor = TakeFirst()
+
+			product_loader.add_css('title', '.s-access-title::text')
+			product_loader.add_css('link', 'a.s-access-detail-page::attr(href)')
+			product_loader.add_css('price', '.s-price::text')
+
+			print('\n')
+
+			yield product_loader.load_item()
